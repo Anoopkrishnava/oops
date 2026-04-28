90398ac3b376c47dab0f106a7cf8cfa10ac26c0a
Anoopkrishnava <anoopkrishnava@gmail.com>
2026-04-28 17:25:40 +0530

    Add files via upload

---
+++ graphicstest/graphics/Square.java
@@ -0,0 +1,14 @@
+package graphics;
+
+public class Square implements Shape {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+        return side * side;
+    }
+}
